f13a2731eae5c6c642d4d7e7f27a51ac9e049367
koreanssam <121275608+koreanssam@users.noreply.github.com>
2024-12-13 22:20:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.title("🤖 생기부 챗봇")
 st.markdown(
     """
     <div style='display: flex; justify-content: center; align-items: center; margin-top: 40px; margin-bottom: 40px;'>
-        <div style='background-color: #e0e0e0; border-radius: 15px; padding: 25px; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15); max-width: 450px; text-align: center;'>
+        <div style='background-color: #f5f5f5; border-radius: 15px; padding: 25px; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15); max-width: 600px; text-align: center;'>
             <strong style='font-size: 18px; color: #222;'>경남교육청 국어 교사 이성원이 만들었습니다.</strong>
             <br>
             <a href='mailto:koreanssam@koreanssam.kr' style='font-size: 15px; color: #0056b3; text-decoration: none;'>koreanssam@koreanssam.kr</a>
